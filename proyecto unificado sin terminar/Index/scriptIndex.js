961b2a642d2fb48730fe36a34edf54f5c31fdaba
SanSin1993 <128734650+SanSin1993@users.noreply.github.com>
2023-04-13 20:11:26 -0300

    Update scriptIndex.js

--- proyecto unificado sin terminar/Index/scriptIndex.js
+++ proyecto unificado sin terminar/Index/scriptIndex.js
@@ -1,4 +1,10 @@
+import Swal from 'sweetalert2';
 $("#botonSacarEntrada").click(function() {
-    alert("Elige con sabiduría");
-    window.location.href = "../Cartelera/cartelera.html";
+    // Muestra un mensaje de SweetAlert
+    Swal.fire({
+        title: '¡Elige con sabiduría!',
+        icon: 'info',
+        confirmButtonText: 'Aceptar',
+    });
+    window.location.href = '../Cartelera/cartelera.html';
 });
